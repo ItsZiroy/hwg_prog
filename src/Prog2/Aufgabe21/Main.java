78d7fabd0665cfc0903b1e21123f562d54eede60
Yannik Hahn <yannihahn@t-online.de>
2022-05-13 12:31:14 +0200

    [Prog2] Aufgabe 21

---
+++ src/Prog2/Aufgabe21/Main.java
@@ -0,0 +1,35 @@
+package Prog2.Aufgabe21;
+
+import Prog2.Aufgabe20.Student;
+import com.google.common.base.Splitter;
+import com.google.common.collect.HashMultimap;
+
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            List<String> lines = Files.readLines(new File("src/Prog2/Aufgabe21/studenten.txt"), StandardCharsets.UTF_8);
+            HashMultimap<String, Student> lastNameMap = HashMultimap.create();
+            Map<Integer,Student> idMap = new HashMap<>();
+            lines.forEach(line -> {
+                List<String> vars = Splitter.on(';').splitToList(line);
+                Student student = new Student(Integer.parseInt(vars.get(0)), vars.get(1), vars.get(2));
+                lastNameMap.put(student.nachname(), student);
+                idMap.put(student.matrikelNr(), student);
+            });
+
+            System.out.println(idMap.get(123456));
+            System.out.println(lastNameMap.get("König"));
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+    }
+}
